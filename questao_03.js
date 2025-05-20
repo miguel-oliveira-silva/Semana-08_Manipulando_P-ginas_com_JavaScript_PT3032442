3f3b7b4e63c6d2fbeebc0097330d11373a521b86
miguel-oliveira-silva <146590724+miguel-oliveira-silva@users.noreply.github.com>
2025-05-20 20:57:00 -0300

    Create questao_03.js

---
+++ questao_03.js
@@ -0,0 +1,25 @@
+let nome = "João da Silva";
+let nota = 7;
+let nota2 = 8.5;
+let notaf = nota * 0.6;
+let notaf2 = nota2 * 0.4;
+let notaFinal = notaf + notaf2;
+alert("Nome do aluno: " + nome + "\nNota 1: " + nota + "\nNota 2: " + nota2);
+alert("Nome do aluno: " + nome + "\nNota final: " + notaFinal);
+
+
+// Aluno 2
+let nome2 = "Miguel Oliveira";
+let nota2_1 = 9;
+let nota2_2 = 7.5;
+let notaFinal2 = nota2_1 * 0.6 + nota2_2 * 0.4;
+alert("Aluno: " + nome2 + "\nNota 1: " + nota2_1 + "\nNota 2: " + nota2_2);
+alert("Nota final de " + nome2 + ": " + notaFinal2);
+
+// Aluno 3
+let nome3 = "Giovanna Menezes";
+let nota3_1 = 6;
+let nota3_2 = 8;
+let notaFinal3 = nota3_1 * 0.6 + nota3_2 * 0.4;
+alert("Aluno: " + nome3 + "\nNota 1: " + nota3_1 + "\nNota 2: " + nota3_2);
+alert("Nota final de " + nome3 + ": " + notaFinal3);
